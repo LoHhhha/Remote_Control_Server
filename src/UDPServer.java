a4340e2c47f5b23c67d50b2aa659c23cb410169f
LoHhhha <1185604794@qq,com>
2023-02-05 16:11:24 +0800

    Initial commit

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -13,14 +13,14 @@ import java.net.InetAddress;
 
 import java.awt.event.KeyEvent;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class UDPServer {
     static private Robot robot;
-    static final byte[] replyMessage = "OK!".getBytes();
+    static final String replyMessage = "OK!";
 
     public static void main(String[] args) {
         try {
@@ -30,9 +30,8 @@ public class UDPServer {
             return;
         }
         byte[] buf = new byte[1024];
-
-        try (DatagramSocket socket = new DatagramSocket(8888)) {
-            System.out.println("Server Waiting...");
+        System.out.println("Server Waiting...");
+        try (DatagramSocket socket = new DatagramSocket(8888);) {
             //socket.setSoTimeout(2000);
             while (true) {
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
@@ -41,35 +40,57 @@ public class UDPServer {
                 } catch (java.net.SocketTimeoutException e) {
                     System.out.println("TimeOut.");
                     continue;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(-1);
                 }
-                new Thread(() -> {
-                    String data = new String(buf, 0, packet.getLength());
-                    InetAddress clientIP = packet.getAddress();
-                    int clintPort = packet.getPort();
-//                    System.out.println("Data: " + data);
-//                    System.out.println("\tFrom: " + clientIP + ':' + clintPort);
-                    DatagramPacket replyPacket = new DatagramPacket(replyMessage, replyMessage.length, clientIP, clintPort);
-                    try {
-                        socket.send(replyPacket);
-                    } catch (IOException e) {
-
-                        throw new RuntimeException(e);
-                    }
-                    //System.out.println("\tReply Complete.");
-                    try{
-                        checkAndDo(data);
-                    }
-                    catch (Exception e){
-                        System.out.println("Error data :" + data);
-                    }
-                }).start();
+                String data = new String(buf, 0, packet.getLength());
+                InetAddress clientIP = packet.getAddress();
+                int clientPort = packet.getPort();
+                //System.out.println("Data: " + data);
+                //System.out.println("\tFrom: " + clientIP + ':' + clientPort);
+                new Thread(new solve(data, clientIP, clientPort)).start();
             }
+
         } catch (Exception e) {
             e.printStackTrace();
+            System.exit(-1);
+        }
+
+    }
+
+    static private class solve implements Runnable {
+        String data;
+        InetAddress clientIP;
+        int clientPort;
+
+        public solve(String message, InetAddress ip, int port) {
+            data = message;
+            clientIP = ip;
+            clientPort = port;
+        }
+
+        @Override
+        public void run() {
+            String reply=null;
+            try {
+                reply = checkAndDo(data);
+            } catch (Exception e) {
+                //System.out.println("<-*Error*-> data :" + data + ", Exception: " + e);
+            }
+            if(reply!=null){
+                byte[]sendData=reply.getBytes();
+                DatagramPacket replyPacket = new DatagramPacket(sendData, sendData.length, clientIP, clientPort);
+                try (DatagramSocket socket = new DatagramSocket()) {
+                    socket.send(replyPacket);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 
-    static public void checkAndDo(String data) {
+    static public String checkAndDo(String data) {
         if (data.startsWith("\\")) {
             if (data.startsWith("\\M")) {
                 PointerInfo pInfo = MouseInfo.getPointerInfo();
@@ -85,6 +106,12 @@ public class UDPServer {
                         robot.mousePress(InputEvent.BUTTON3_MASK);
                         robot.mouseRelease(InputEvent.BUTTON3_MASK);
                     }
+                    case "\\M_d"->{
+                        robot.mouseWheel(1);
+                    }
+                    case "\\M_u"->{
+                        robot.mouseWheel(-1);
+                    }
                     case "\\M_ld" -> robot.mousePress(InputEvent.BUTTON1_MASK);
                     case "\\M_lu" -> robot.mouseRelease(InputEvent.BUTTON1_MASK);
                     case "\\M_rd" -> robot.mousePress(InputEvent.BUTTON3_MASK);
@@ -96,6 +123,7 @@ public class UDPServer {
                             int X = Integer.parseInt(nums[0]), Y = Integer.parseInt(nums[1]);
                             robot.mouseMove(mx + X, my + Y);
                         }
+                        return null;
                     }
                 }
             } else if (data.startsWith("\\K")) {
@@ -121,9 +149,12 @@ public class UDPServer {
                         robot.keyRelease(KeyEvent.VK_ESCAPE);
                     }
                 }
+            } else if(data.startsWith("\\C")){
+                data=data.substring(3);
+                return getResultAndReply(data);
             } else if (data.equals("\\exit")) {
-                System.out.println("Server Power Off.1");
-                System.exit(1);
+                System.out.println("Server Power Off.");
+                System.exit(0);
             }
         } else {
             setClipboardString(data);
@@ -132,6 +163,20 @@ public class UDPServer {
             robot.keyRelease(KeyEvent.VK_CONTROL);
             robot.keyRelease(KeyEvent.VK_V);
         }
+        return replyMessage;
+    }
+
+    public static String getResultAndReply(String commend) {
+        Runtime runtime = Runtime.getRuntime();
+        String res="";
+        try {
+            String[] order = {"cmd", "/c", commend};
+            InputStream ips = runtime.exec(order).getInputStream();
+            res=new BufferedReader(new InputStreamReader(ips, Charset.forName("GBK"))).lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            res=e.getMessage();
+        }
+        return res;
     }
 
     public static void setClipboardString(String text) {
